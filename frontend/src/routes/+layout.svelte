124ef16d0d1c1cf07623870f20dc434f485c1963
Daniel Lipniacki <daniel.lipniacki@protonmail.com>
2025-01-31 23:01:30 +0100

    First home page (#1)
    * home page
    
    * fixed navigation with tabs
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+
 	let { children } = $props();
 </script>
 
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+    import AnimatedGradient from "$lib/components/AnimatedGradient.svelte";
+</script>
+<div class="container flex flex-col items-center justify-center w-full h-screen max-w-none px-0">
+    <AnimatedGradient 
+        duration="8s"
+        colors={{
+            from: "#8B5CF6",
+            via: "#EC4899",
+            to: "#EF4444"
+        }}
+        degree="-45deg"
+    > 
+        <div class="flex flex-col items-center justify-center">
+            <h1 class="text-4xl">Janko Muzykant</h1>
+            <div class="mt-10"></div>
+            <div class="flex gap-2 w-full">
+                <a href="/auth" class="w-full">
+                    <Button class="w-full">Login</Button>
+                </a>
+                <a href="/signup" class="w-full">
+                    <Button class="w-full">Signup</Button>
+                </a>
+            </div>
+        </div>
+    </AnimatedGradient>
+</div>
